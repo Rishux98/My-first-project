8bbfd17d0bcfce823323f60268622eceb9e437e0
Rishux98 <rishavraj74799#gmail.com>
2025-12-28 18:15:48 +0530

    Fixed CSS for YouTube background

--- src/components/TransitionScreen.jsx
+++ src/components/TransitionScreen.jsx
@@ -41,17 +41,25 @@ const TransitionScreen = () => {
         </button>
       )}
 
-      <video 
-        ref={videoRef}
-        autoPlay 
-        muted 
-        playsInline 
-        className="fullscreen-video"
-        onEnded={handleVideoEnd}
-      >
-        <source src={dropVideo} type="video/mp4" />
-      </video>
-
+    <div className="video-background">
+        <iframe 
+          src="https://www.youtube.com/embed/W_bCS8M5S9c?autoplay=1&mute=1&loop=1&playlist=W_bCS8M5S9c&controls=0&showinfo=0&rel=0&iv_load_policy=3&fs=0" 
+          title="Infinity Castle Background"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: '100vw',
+            height: '100vh',
+            transform: 'translate(-50%, -50%)',
+            objectFit: 'cover',
+            zIndex: '-1',
+            pointerEvents: 'none',
+          }}
+          frameBorder="0"
+          allow="autoplay; encrypted-media" 
+        ></iframe>
+      </div>
       {showButton && (
         <div className="overlay-content">
           <h1>YOU HAVE FALLEN.</h1>
